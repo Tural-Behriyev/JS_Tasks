1495df7c523036d3aa0a1c83aad48f0e303904f3
Tural Behriyev <tbehriyev87@gmail.com>
2024-02-08 17:20:37 +0400

    first commit

---
+++ task-8/js/main.js
@@ -0,0 +1,9 @@
+let squareHpme = +prompt("enter the square footage of the home");
+let price = +prompt("enter the price of the house");
+let currency = prompt("enter currency");
+
+if (squareHpme > 2000 && price === 99.999 && currency === "azn") {
+  console.log("the house is yours");
+} else {
+  console.log("look for a new home");
+}
