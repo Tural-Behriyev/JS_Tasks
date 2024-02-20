6e0e8f2ad1db92e9b4a9047903ada79436c4b45c
Tural Behriyev <tbehriyev87@gmail.com>
2024-02-20 16:38:37 +0400

    new update

---
+++ task-8/js/main.js
@@ -0,0 +1,129 @@
+// Tapşırıq - 01;
+
+// const user = {
+//   name: "John",
+//   gender: "male",
+//   birthday: "1999-05-29",
+// };
+// console.log("Doğum tarihi: " + user.birthday);
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+// Tapşırıq - 02;
+
+// const istifadeci = {
+//   ad: "John",
+//   soyad: "Doe",
+//   yas: 25,
+//   dogum_tarixi: "1999-05-15"
+// };
+// istifadeci.yas += 1;
+// delete istifadeci.dogum_tarixi;
+// console.log(istifadeci);
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+
+// Tapşırıq - 03;
+
+// class Person {
+//   constructor(name, age, retirementAge) {
+//     this.name = name;
+//     this.age = age;
+//     this.retirementAge = retirementAge;
+//   }
+//   setAge(newAge) {
+//     this.age = newAge;
+//   }
+//   getYearsBeforeRetirement() {
+//     const yearsLeft = this.retirementAge - this.age;
+//     if (yearsLeft > 0) {
+//       return yearsLeft;
+//     } else {
+//       return 'Zaten emekli oldunuz!';
+//     }
+//   }
+// }
+// const person1 = new Person('John', 40, 65);
+// person1.setAge(50);
+// console.log(person1.getYearsBeforeRetirement());
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+// Tapşırıq - 04;*******************************
+
+// const person = {
+//   name: "John",
+//   age: 30,
+//   retirementAge: 65,
+//   setAge: function (newAge) {
+//     this.age = newAge;
+//   },
+//   getYearsBeforeRetirement: function () {
+//     let yearsLeft = this.retirementAge - this.age;
+//     if (yearsLeft >= 0) {
+//       return yearsLeft;
+//     } else {
+//       return "Artıq pensiyaya düşmüşsünüz!";
+//     }
+//   },
+// };
+// console.log("İlk yaş: " + person.age);
+// console.log("Qalan əmək haqqını hesablayın: " + person.getYearsBeforeRetirement());
+// person.setAge(40);
+// console.log("Yeni yaş: " + person.age);
+// console.log("Qalan əmək haqqını hesablayın: " + person.getYearsBeforeRetirement());
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+
+// Tapşırıq - 05;
+
+// let kalkulyator = {
+//     toplama: function(x, y) {
+//         return x + y;
+//     },
+//     cixma: function(x, y) {
+//         return x - y;
+//     },
+//     vurma: function(x, y) {
+//         return x * y;
+//     },
+//     bolme: function(x, y) {
+//         if (y === 0) {
+//             return "sıfıra bölmə yoxdur";
+//         } else {
+//             return x / y;
+//         }
+//     }
+// };
+// console.log("Toplama: " + kalkulyator.toplama(5, 3));
+// console.log("Çıxma: " + kalkulyator.cixma(5, 3));
+// console.log("Vurma: " + kalkulyator.vurma(5, 3));
+// console.log("Bölmə: " + kalkulyator.bolme(10, 2));
+// console.log("Bölmə: " + kalkulyator.bolme(5, 0));
+
+
+
+/////////////////////////////////////////////////////////////////////////////
