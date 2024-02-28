cfe5ac677392dc995e0537df7a42bed8d606d1fb
Tural Behriyev <tbehriyev87@gmail.com>
2024-02-28 17:29:42 +0400

    updates

---
+++ task-11/main.js
@@ -0,0 +1,36 @@
+//?Task1
+
+// let qruplar = ["Alpha", "Beta", "Gamma"];
+// let yoldaş = "Delta";
+// qruplar.push(yoldaş);
+// qruplar.shift();
+// console.log(yoldaş);
+// document.write(yoldaş)
+
+//////////////////////////////////////////////////////////////
+
+//?Task2
+
+// let myArray = ["Element1", "Element2", "Element3", "Element4"];
+// myArray[1] = "Classified";
+// console.log(myArray);
+
+//////////////////////////////////////////////////////////////
+
+//?Task4
+
+// let input = prompt("Lütfən beş ədəd daxil edin (vergüllə ayrılmış):");
+// let numbers = input.split(",");
+// let smallest = Math.min(...numbers);
+// console.log(smallest);
+// document.write(smallest);
+
+//////////////////////////////////////////////////////////////
+
+//?Task5
+
+// let text = "32, 31, 34, 36, 31";
+// let replacedText = text.split(", ").join(";");
+// console.log(replacedText);
+
+//////////////////////////////////////////////////////////////
